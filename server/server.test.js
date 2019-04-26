8de4de4a5ea2220a6a53026a9d6020cc7e605826
Michael Linderman <mlinderman@middlebury.edu>
2019-04-25 20:42:59 -0400

    Ensure .env file picked up in tests

--- server/server.test.js
+++ server/server.test.js
@@ -3,3 +3,7 @@ const { app } = require('./server');
 test('Server "smoke" test', () => {
   expect(app).toBeDefined();
 });
+
+test('dotenv configured', () => {
+  expect(process.env.MY_SECRET).toBeDefined();
+});
